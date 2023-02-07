1d14791a3feb1499684273b720176f46b89e5119
Felix Reiniger <felix.reiniger@ost.ch>
2023-02-07 12:51:22 +0100

    Fix Delte Button

--- www/static/audit.js
+++ www/static/audit.js
@@ -705,7 +705,7 @@ var new_tag_id = 0;
 
 function addNewTag(e) {
   var plap = $('#new_tags');
-  var new_value = '<tr><th><input type="text" id="new_tag_key_' + new_tag_id + '"/></th><td><input type="text" id="new_tag_value_' + new_tag_id + '"/><button type="button" id="delete_' + new_tag_id + '" onclick="document.getElementById(\'new_tags\').deleteRow(' + new_tag_id + ')">Delete</button></td></tr>';
+  var new_value = '<tr id="tag_row_'+ new_tag_id +'"><th><input type="text" id="new_tag_key_' + new_tag_id + '"/></th><td><input type="text" id="new_tag_value_' + new_tag_id + '"/><button type="button" id="delete_' + new_tag_id + '" onclick="document.getElementById(\'tag_row_' + new_tag_id + '\').remove()">Delete</button></td></tr>';
   plap.append(new_value);
 
   var delete_btn = 'delete_' + new_tag_id;
